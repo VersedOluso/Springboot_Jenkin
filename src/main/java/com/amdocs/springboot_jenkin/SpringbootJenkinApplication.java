ef62d28804d17b3068d0c979222393a79bb6fb15
VersedOluso <vedant.h132000@gmail.com>
2023-09-26 11:51:55 +0530

    Commiting Here

---
+++ src/main/java/com/amdocs/springboot_jenkin/SpringbootJenkinApplication.java
@@ -0,0 +1,13 @@
+package com.amdocs.springboot_jenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amdocs/springboot_jenkin/SpringbootJenkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amdocs.springboot_jenkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
